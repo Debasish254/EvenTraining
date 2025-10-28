b02d6bbf084dbce7b423a39cc5eb9cfb5ad5d30b
Debasish254 <debasishrath978@gmail.com>
2025-10-28 22:08:14 +0530

    Add java file

---
+++ Amstro.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Amstro{
+    public static void main(String[]args){
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int Num=Sc.nextInt();
+        int orginal=Num;
+        int count=0;
+        int sum=0;
+        int temp=Num;
+        while(temp>0){
+            count++;
+            temp=temp/10;
+        }
+        temp=Num;
+        while(temp>0){
+            int digit=temp%10;
+            sum+=(int)Math.pow(digit,count);
+            temp=temp/10;
+        }
+        if(sum==orginal){
+            System.out.println(+Num+" Number is Amstrong Number");
+        }
+        else{
+        System.out.println(+Num+" Number is  Not Amstrong Number");
+    }
+}
+}
