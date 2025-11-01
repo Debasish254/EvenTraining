c6410df4382972ae54e80dabeae041de34969d00
Debasish254 <debasishrath978@gmail.com>
2025-11-01 22:50:28 +0530

    Add java File

---
+++ ArrayCount.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ArrayCount{
+ public static void main (String[]args){
+     Scanner Sc=new Scanner(System.in);
+     int arr[]={60,50,40,40,30,20,80,20,20};
+    int length=arr.length;
+     int key=Sc.nextInt();
+        int count=0;
+     for(int i=0;i<length;i++){
+       if(key==arr[i]){
+       count++;
+       
+    
+    }
+    
+}
+System.out.println("My key value is " +count+" times appears"); 
+}
+}
+ 
